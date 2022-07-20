e0055bd3fb1723ec2e96c8418b1f0e36c212a4db
Willian Coelho da Cunha <willian_comp@outlook.com>
2022-07-20 01:22:12 -0300

    chore(marketplace): add service workers to the marketplace application

--- projects/marketplace/src/app/app/app.module.ts
+++ projects/marketplace/src/app/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { environment } from '../../environments/environment';
 
 import { ButtonModule } from '@will/ui-core/src/lib/button';
 import { EmailFormFieldModule } from '@will/ui-core/src/lib/email-form-field';
@@ -32,7 +35,13 @@ import { LatestRecomendationsComponent } from './latest-recomendations/latest-re
     AppRoutingModule,
     ButtonModule,
     EmailFormFieldModule,
-    PasswordFormFieldModule
+    PasswordFormFieldModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [
